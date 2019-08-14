5fe1cfffc13f088043278310010931cce287a8dc
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-08-14 15:47:53 -0300

    :card_file_box: Add validation to user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,32 @@
-'use strict';
+import bcrypt from 'bcryptjs'
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: 'Invalid e-mail. Please insert a valid one'
+        }
+      }
+    },
     password: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
+  }, {
+    hooks: {
+      async beforeCreate(user) {
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    }
+  });
+  
+  User.associate = function(models) {};
+
+  User.prototype.passwordMatches = async function(password) {
+    return bcrypt.compare(password, this.password);
   };
   return User;
 };
